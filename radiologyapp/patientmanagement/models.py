7f74e155c9029c84b3fa49fbb8dcabee5a19ff29
ruchita20 <ruchita@berkeley.edu>
2014-07-24 14:07:23 +0530

    .gitignore and models.py for the patientmanagement

--- radiologyapp/patientmanagement/models.py
+++ radiologyapp/patientmanagement/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Patients(models.Model):
+
+    first_name = models.CharField(
+        max_length=255,
+    )
+    last_name = models.CharField(
+        max_length=255,
+
+    )
+    ref_doc = models.CharField(
+        max_length=255,
+
+    )
+    phone = models.IntegerField()	
+    email = models.EmailField()
+
+    def __str__(self):
+
+        return ' '.join([
+            self.first_name,
+            self.last_name,
+        ])
